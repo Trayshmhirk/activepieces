844a165a6931c4946cf9c6913347b09bfedb9e7d
Frank Micheal <harlex.mikkey@gmail.com>
2024-09-14 03:11:04 +0000

    feat(upgradechat): add or update contact, create invoice, and 4 actions

--- packages/pieces/community/upgradechat/src/index.ts
+++ packages/pieces/community/upgradechat/src/index.ts
@@ -3,6 +3,12 @@ import {
   PieceAuth,
   Property,
 } from '@activepieces/pieces-framework';
+import { addOrUpdateContactExtended } from './lib/actions/add-or-update-contact-extended';
+import { addOrUpdateContact } from './lib/actions/add-or-update-contact';
+import { addOrUpdateSubscription } from './lib/actions/add-or-update-subscription';
+import { createInvoice } from './lib/actions/create-invoice';
+import { createProduct } from './lib/actions/create-product';
+import { getContactDetails } from './lib/actions/get-contact-details';
 
 const markdownDescription = `
   Follow these instructions to get your Upgrade.chat API Key:
@@ -11,7 +17,7 @@ const markdownDescription = `
   2. Once on the website, locate and click on the admin to obtain your Upgrade.chat API Key.
 `;
 
-export const upgradechataAuth = PieceAuth.CustomAuth({
+export const upgradechatAuth = PieceAuth.CustomAuth({
   description: markdownDescription,
   required: true,
   props: {
@@ -44,10 +50,17 @@ export const upgradechataAuth = PieceAuth.CustomAuth({
 export const upgradechat = createPiece({
   displayName: 'Upgrade.chat',
   description: '',
-  auth: upgradechataAuth,
+  auth: upgradechatAuth,
   minimumSupportedRelease: '0.20.0',
   logoUrl: 'https://cdn.activepieces.com/pieces/upgradechat.png',
   authors: ['Trayshmhirk'],
-  actions: [],
+  actions: [
+    addOrUpdateContact,
+    addOrUpdateContactExtended,
+    addOrUpdateSubscription,
+    createInvoice,
+    createProduct,
+    getContactDetails,
+  ],
   triggers: [],
 });
